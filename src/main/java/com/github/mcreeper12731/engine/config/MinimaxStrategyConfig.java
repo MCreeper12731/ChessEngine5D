537354103d5538196d04d69382c00b915e8eb6d8
MCreeper12731 <vitko44@gmail.com>
2026-06-19 09:36:14 +0200

    Removed legacy code

--- src/main/java/com/github/mcreeper12731/engine/config/MinimaxStrategyConfig.java
+++
@@ -1,8 +0,0 @@
-package com.github.mcreeper12731.engine.config;
-
-public record MinimaxStrategyConfig(String solverName, int moveLimit) implements MoveStrategyConfig {
-
-    public MinimaxStrategyConfig(int moveLimit) {
-        this("minimax", moveLimit);
-    }
-}
--- src/main/java/com/github/mcreeper12731/game/moves/TurnPlayedListener.java
+++
@@ -1,7 +0,0 @@
-package com.github.mcreeper12731.game.moves;
-
-public interface TurnPlayedListener {
-
-    void onTurnPlayed(Turn turn);
-
-}
